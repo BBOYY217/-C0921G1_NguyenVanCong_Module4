2dcef02fc5c9d706d816a6fe443db01ee9c6a5de
Công Nguyễn <nguyenvancong2107@gmail.com>
2022-01-17 07:30:21 +0700

    upbaimoi

---
+++ bai3_data_binding_and_form/exercise/configuring_email/src/main/java/com/codegym/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.codegym.controller;
+
+import com.codegym.model.Email;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/email")
+public class EmailController {
+
+    @GetMapping("")
+    public String showEmail (Model model) {
+        model.addAttribute("email",new Email());
+        return "home";
+    }
+
+    @PostMapping("/update")
+    public ModelAndView update (@ModelAttribute Email email) {
+            return new ModelAndView("show","email",email);
+    }
+}
---
+++ bai3_data_binding_and_form/exercise/configuring_email/src/main/webapp/WEB-INF/views/show.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ADMIN
+  Date: 1/15/2022
+  Time: 9:08 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+Language : ${email.languages}
+Size : ${email.pageSize}
+Spam : ${email.spamFilter}
+Signature : ${email.signature}
+</body>
+</html>
